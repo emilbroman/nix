fef24ad20899ad6766f9e66ef15d87a88141cf5b
Emil Broman <emil@emilbroman.me>
2025-02-16 17:22:16 +0100

    Only track last bootloader

--- flake.nix
+++ flake.nix
@@ -134,6 +134,7 @@
 
           # Use the systemd-boot EFI boot loader.
           boot.loader.systemd-boot.enable = true;
+          boot.loader.systemd-boot.configurationLimit = 1;
           boot.loader.efi.canTouchEfiVariables = true;
 
           time.timeZone = "Europe/Stockholm";

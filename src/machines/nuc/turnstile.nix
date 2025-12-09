2b215d435ad24ae7333274b023fae3537133f37c
Emil Broman <emil@emilbroman.me>
2025-12-09 18:19:35 +0100

    Use turnstile config

--- src/machines/nuc/turnstile.nix
+++ src/machines/nuc/turnstile.nix
@@ -3,12 +3,26 @@
     reverse_proxy http://127.0.0.1:7571
   '';
 
+  environment.etc."turnstile/turnstile.toml".text = ''
+    [database]
+    sqlite = "/var/lib/turnstile/turnstile.db"
+
+    [server]
+    advertise-url = "https://id.bb3.site"
+    cookie-domain = "bb3.site"
+    hostname = "127.0.0.1"
+
+    [pki]
+    "ca.crt" = "/var/lib/turnstile/ca.crt"
+    "ca.key" = "/var/lib/turnstile/ca.key"
+  '';
+
   systemd.services.turnstile = {
     description = "Turnstile";
     wantedBy = ["multi-user.target"];
 
     serviceConfig = {
-      ExecStart = "/home/emilbroman/code/turnstile/turnstile serve --db /var/lib/turnstile/turnstile.db --advertise-url https://id.bb3.site --domain bb3.site --ca /var/lib/turnstile/ca.crt --private-key-pem-file /var/lib/turnstile/ca.key";
+      ExecStart = "/home/emilbroman/code/turnstile/turnstile serve --config /etc/turnstile/turnstile.toml";
       User = "emilbroman";
       Group = "wheel";
 

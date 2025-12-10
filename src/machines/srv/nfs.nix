c034132dcfc236d84a7d4ad5c812c6fd776d1746
Emil Broman <emil@emilbroman.me>
2025-12-10 21:20:18 +0100

    Simplify NFS

--- src/machines/srv/nfs.nix
+++ src/machines/srv/nfs.nix
@@ -1,6 +1,6 @@
 {
-  fileSystems."/nfs/pvc" = {
-    device = "/var/lib/pvc";
+  fileSystems."/nfs" = {
+    device = "/var/lib/tempnfs";
     options = ["bind"];
     fsType = "none";
   };
@@ -8,6 +8,5 @@
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /nfs       10.0.0.0/8(rw,sync,fsid=0,crossmnt,no_subtree_check,insecure) 127.0.0.0/8(rw,sync,fsid=0,crossmnt,no_subtree_check,insecure)
-    /nfs/pvc   10.0.0.0/8(rw,sync,no_subtree_check,insecure) 127.0.0.0/8(rw,sync,no_subtree_check,insecure)
   '';
 }

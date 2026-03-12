eb9ac1ca46467c0f707bd5d66e84b069b756a99d
Emil Broman <emil@emilbroman.me>
2026-03-12 15:28:36 +0100

    feat(agents): add some Claude Code stuff

--- src/agents/AGENTS.md
+++ src/agents/AGENTS.md
@@ -1,4 +1,9 @@
 You are an AI agent writing code on the behalf of Emil. That's me.
 
-Here are some skills you should always make use of:
+Here are some skills you should always make use of, without hesitating or asking:
 - nix-tooling
+
+# Git Style
+Use semantic commit messages, unless I tell you otherwise, or that you see in the repository history that another style is used. If you are unsure, ask me.
+
+Always try to split up the commits per package, if in a monorepo. The order of such commits should be that the dependencies are committed before the dependents.
--- src/agents/flake.nix
+++ src/agents/flake.nix
@@ -1,6 +1,10 @@
 {
   outputs = {self}: {
-    home-module = {pkgs, ...}: {
+    home-module = {
+      pkgs,
+      lib,
+      ...
+    }: {
       home.packages = with pkgs; [
         codex
         claude-code
@@ -11,6 +15,36 @@
 
       home.file.".claude/skills".source = ./skills;
       home.file.".codex/skills".source = ./skills;
+
+      home.file.".claude/settings.json".text = builtins.toJSON {
+        enabledPlugins = {
+          "tvm-aws-sso@tvm" = true;
+        };
+        extraKnownMarketplaces = {
+          tvm.source = {
+            source = "github";
+            repo = "TV4/claude-plugins";
+          };
+        };
+        hooks = {
+          Notification = [
+            {
+              matcher = "";
+              hooks = [
+                {
+                  type = "command";
+                  command = lib.strings.concatStringsSep " " [
+                    (lib.getExe pkgs.terminal-notifier)
+                    "-title 'Claude Code is waiting'"
+                    "-message \"in \${CLAUDE_PROJECT_DIR/\$HOME/~}\""
+                    "-sound Glass"
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+      };
     };
   };
 }
